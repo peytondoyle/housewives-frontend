03a4807fa098b0ce57673b00671c57d58325be7a
Peyton <peyton.doyle@icloud.com>
2021-11-05 14:50:52 -0400

    Revert "Coming soon test" This reverts commit 77508e71dab32048b61d2753c33a3597a5ef72d5.

--- src/App.js
+++ src/App.js
@@ -14,9 +14,20 @@ function App() {
 
   return (
     <div className="App">
-      <br></br>
-      <br></br>
-       <p>housewives.app<br></br>coming soon.</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
